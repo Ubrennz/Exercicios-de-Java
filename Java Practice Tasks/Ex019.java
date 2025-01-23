f6d60887c2e986cb63b10770dff62897d578dd27
Brenno Oliveria <118616498+Ubrennz@users.noreply.github.com>
2025-01-23 17:05:39 -0300

    Contagem regressiva

---
+++ Java Practice Tasks/Ex019.java
@@ -0,0 +1,7 @@
+public class Ex019 {
+    public static void main(String[] args) {
+        for (int i=6; i>-1; i--) {
+            System.out.println("Contagem regressiva de i: " + i);
+        }
+    }
+}
