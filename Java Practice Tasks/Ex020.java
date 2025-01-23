e3904f3d025712131c5a5fc6bef58dd88dcd062d
Brenno Oliveria <118616498+Ubrennz@users.noreply.github.com>
2025-01-23 17:07:19 -0300

    Contagem de 1 até 50

---
+++ Java Practice Tasks/Ex020.java
@@ -0,0 +1,9 @@
+public class Ex020 {
+    public static void main(String[] args) {
+        for (int i=1; i<51; i++) {
+            System.out.print(i + ", ");            
+        }
+
+        System.out.println("Acabou!");
+    }
+}
