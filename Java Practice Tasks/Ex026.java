70193d4f4c739bd7490d4a0e7d8537e782813307
Brenno Oliveria <118616498+Ubrennz@users.noreply.github.com>
2025-02-05 18:44:10 +0000

    Usando instancias de classe

--- Java Practice Tasks/Ex026.java
+++ Java Practice Tasks/Ex026.java
@@ -1,7 +1,7 @@
-import entities.Triangulo;
 import java.util.Scanner;
+import entities.Triangulo;
 
-public class Aula19 {
+public class Aula20 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Triangulo x, y;
@@ -19,11 +19,8 @@ public class Aula19 {
         y.b = sc.nextInt();
         y.c = sc.nextInt();
 
-        double p1 = (x.a + x.b + x.c) / 2.0;
-        double areaX = Math.sqrt(p1 * (p1 - x.a) * (p1 - x.b) * (p1 - x.c));
-
-        double p2 = (y.a + y.b + y.c) / 2.0;
-        double areaY = Math.sqrt(p2 * (p2 - y.a) * (p2 - y.b) * (p2 - y.c));
+        double areaX = x.calculandoArea();
+        double areaY = y.calculandoArea();
 
         if (areaX > areaY) {
             System.out.println("A area do triangulo X é maior do que a do Y");
