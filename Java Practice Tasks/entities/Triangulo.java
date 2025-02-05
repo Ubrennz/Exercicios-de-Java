617e5871fd6bc4221e37597736d73e9cdcab7e96
Brenno Oliveria <118616498+Ubrennz@users.noreply.github.com>
2025-02-05 18:45:06 +0000

    Usando metodo para calcular a area

--- Java Practice Tasks/entities/Triangulo.java
+++ Java Practice Tasks/entities/Triangulo.java
@@ -3,5 +3,11 @@ package entities;
 public class Triangulo {
     public double a;
     public double b;
-    public double c;    
+    public double c;
+
+    public double calculandoArea() {
+        double p = (a + b + c) / 2.0;
+        double area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        return area;
+    }
 }
