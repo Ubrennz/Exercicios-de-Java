119b89f662c1997b1f29c0adf68ee7920be8971c
Brenno Oliveria <118616498+Ubrennz@users.noreply.github.com>
2025-02-08 22:13:16 +0000

    Update Ex028.java

--- Java Practice Tasks/Ex028.java
+++ Java Practice Tasks/Ex028.java
@@ -1,28 +1,33 @@
 import java.util.Scanner;
 import entities.Produto;
 
-public class Ex028 {
+public class Aula21 {
    public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Produto x = new Produto();
 			
 		System.out.print("Digite o nome do produto: ");
 		x.nome = sc.nextLine();
-		System.out.print("Digite o preço do produto: ");
+		System.out.print("Digite o preço do produto: R$");
 		x.preco = sc.nextDouble();
 		System.out.printf("Digite a quantidade do produto %s no estoque: ", x.nome);
-		x.quantidade = sc.nextInt();  
-		
-		System.out.printf("Produto: %s%nPreço: %.2f%nQuantidade: %d%n", x.nome, x.preco, x.quantidade);
-        	System.out.println("Valor total: " + x.totalValorEstoque());
+		x.quantidade = sc.nextInt();
+
+		System.out.println(x.toString());	
 
 		System.out.print("Digite a quantidade para ser adicionado ao estoque: ");
 		int addEstoque = sc.nextInt();
 		x.addQtde(addEstoque);
-		
+
+		System.out.println("Lista atualizada: ");
+		System.out.println(x.toString());
+
 		System.out.print("Digite a quantidade para ser removido do estoque: ");
 		int valorRemovido = sc.nextInt();
-		x.removerQtde(valorRemovido);		
+		x.removerQtde(valorRemovido);
+		
+		System.out.println("Lista atualizada: ");
+		System.out.println(x.toString());
 		
 		sc.close();
    	}
