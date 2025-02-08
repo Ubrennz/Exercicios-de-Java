4369b5c950225560d499f991fdd2d9b05003463e
Brenno Oliveria <118616498+Ubrennz@users.noreply.github.com>
2025-02-08 22:12:19 +0000

    Update Produto.java

--- Java Practice Tasks/entities/Produto.java
+++ Java Practice Tasks/entities/Produto.java
@@ -10,15 +10,17 @@ public class Produto {
         return total;
     }
 
-    public void addQtde(int valorParaSerAdd) {
-        quantidade += valorParaSerAdd;
-        System.out.printf("Lista atualiada\nProduto: %s%nPreço: %.2f%nQuantidade: %d%n", nome, preco, quantidade);
-        System.out.println("Valor total: " + totalValorEstoque());
+    public void addQtde(int quantidade) {
+        this.quantidade += quantidade;         
     }
 
-    public void removerQtde(int valorParaSerRemovido) {
-        quantidade -= valorParaSerRemovido;
-        System.out.printf("Lista atualizada\nProduto: %s%nPreço: %.2f%nQuantidade: %d%n", nome, preco, quantidade);
-        System.out.println("Valor total: " + totalValorEstoque());
+    public void removerQtde(int quantidade) {
+        this.quantidade -= quantidade;        
+    }
+
+    public String toString() {
+        return nome + ", R$" + String.format("%.2f", preco) 
+        + ", quantidade: " + quantidade 
+        +", valor total: "  + String.format("%.2f", totalValorEstoque());
     }
 } 
