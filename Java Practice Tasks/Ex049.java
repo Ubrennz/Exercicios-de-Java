c8988e9ff2862df63ca3e59f07b821e4a8fb22de
Brenno Oliveria <118616498+Ubrennz@users.noreply.github.com>
2025-05-08 15:13:30 -0300

    Upcasting and DownCasting

---
+++ Java Practice Tasks/Ex049.java
@@ -0,0 +1,27 @@
+import entities.Conta;
+import entities.ContaNegocios;
+import entities.ContaPoupanca;
+
+public class Ex049 {
+    public static void main(String[] args) {
+        Conta conta = new ContaNegocios(1, "Eduardo", 1230.00, 550.00);
+        ContaNegocios contaNegocios = new ContaNegocios(2, "Ana", 1678.00, 500.00);
+        ContaPoupanca contaPoupanca = new ContaPoupanca(3, "Gustavo", 2010.00, 5.00);
+        Conta conta2 = new ContaPoupanca(4, "Flora", 12345.00, 5.00);
+
+        // Upcasting
+        Conta novaConta = contaNegocios;
+        System.out.println(novaConta);
+
+        Conta novaConta2 = contaPoupanca;
+        System.out.println(novaConta2);
+
+        // DownCasting
+        ContaNegocios novaContaNegocios = (ContaNegocios) conta;
+        System.out.println(novaContaNegocios.toString2());
+
+        ContaPoupanca novaContaPoupanca = (ContaPoupanca) conta2;
+        System.out.println(novaContaPoupanca.toString3());
+
+    }
+}
