9631c03908db7b585783c24776f5fea0c23f2ae4
Brenno Oliveria <118616498+Ubrennz@users.noreply.github.com>
2025-05-08 15:15:58 -0300

    Subclasse da classe Conta

---
+++ Java Practice Tasks/entities/ContaPoupanca.java
@@ -0,0 +1,32 @@
+package entities;
+
+public class ContaPoupanca extends Conta {
+    private Double taxaDeJuros;
+
+    public ContaPoupanca() {
+        super();
+    }
+
+    public ContaPoupanca(Integer numero, String titular, Double saldo, Double taxaDeJuros) {
+        super(numero, titular, saldo);
+        this.taxaDeJuros = taxaDeJuros;
+    }
+
+    public Double getTaxaDeJuros() {
+        return taxaDeJuros;
+    }
+
+    public void setTaxaDeJuros(Double taxaDeJuros) {
+        this.taxaDeJuros = taxaDeJuros;
+    }
+
+    public String toString3() {
+        return "ContaPoupanca{" +
+                "taxaDeJuros=" + taxaDeJuros +
+                '}';
+    }
+
+    public void atualizarSaldo() {
+        saldo += saldo * taxaDeJuros;
+    }
+}
