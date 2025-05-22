a3e4b1352ec46311b06d722450632548446ecb7c
Brenno Oliveria <118616498+Ubrennz@users.noreply.github.com>
2025-05-22 15:34:12 -0300

    superclasse usanda em Ex051

---
+++ Java Practice Tasks/entities/Produto3.java
@@ -0,0 +1,35 @@
+package entities;
+
+public class Produto3 {
+    private String nome;
+    private Double preco;
+
+    public Produto3() {
+
+    }
+
+    public Produto3(String nome, Double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public String etiquetaDoProduto() {
+        return nome + ", R$" + String.format("%.2f", preco);
+    }
+}
