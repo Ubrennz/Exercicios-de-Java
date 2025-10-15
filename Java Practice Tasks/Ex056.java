a1f88651e0b71126c2ef921812b6421a6cde996f
Brenno Oliveria <118616498+Ubrennz@users.noreply.github.com>
2025-10-14 22:48:00 -0300

    Criando arquivo csv

---
+++ Java Practice Tasks/Ex056.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Ex056 {
+    public static void main(String[] args) {
+
+    }
+}
